018a3089e22dbd8ec4a7115accb1f8e63f6046c6
Elvis Espinoza <elvis.espinoza.camargo@gmail.com>
2023-05-15 14:42:46 -0500

    add apiFallback

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: 80, // Reemplaza 3000 con el puerto deseado
-  // },
+  server: {
+    historyApiFallback: true,
+  },
 });
